375849e898a7091135b5c146799c7992bb341928
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-08-29 13:23:37 -0700

    wrote aboutController.reveal function so that it will hide everything but the about section when the function is called

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -3,6 +3,8 @@
 
   aboutController.reveal = function() {
     /* TODO: Use your DOM skills to reveal only the about section! */
+    $('#articles, #ajax-spinner').hide();
+    $('#about').show();
   };
 
   module.aboutController = aboutController;
