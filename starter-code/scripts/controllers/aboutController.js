94de3af1c56434b24c812c4227278ab8e785a9da
bronwyndet <wayward@waywardcoffee.com>
2016-08-29 14:07:17 -0700

    finished all TODOs, reordered controller scripts to reveal section on reload, tested and all working

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -2,9 +2,9 @@
   var aboutController = {};
 
   aboutController.reveal = function() {
-    /* TODO: PARTIALLY DONE: Use your DOM skills to reveal only the about section! */
+    /* TODO: ** DONE **: Use your DOM skills to reveal only the about section! */
     $('#about').show();
-    $('section').not('#about').hide();
+    $('#articles').hide();
   };
 
   module.aboutController = aboutController;
--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -2,9 +2,9 @@
   var articleController = {};
 
   articleController.reveal = function() {
-    /* TODO: PARTIALLY DONE: Use your DOM skills to reveal only the articles section! */
+    /* TODO: ** DONE **: Use your DOM skills to reveal only the articles section! */
     $('#articles').show();
-    $('section').not('#articles').hide();
+    $('#about').hide();
   };
 
   module.articleController = articleController;
